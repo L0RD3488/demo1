5126f013a5cc7d4782aa057e4f6a29233750d64e
L0RD3488 <3488l0rd@gmail.com>
2024-02-26 18:45:48 +0500

    Update and rename server.js to server-example.js
    server-example

--- server-example.js
+++ server-example.js
@@ -1,21 +1,17 @@
-// REST API JSON-DATA GET and POST. (MI-10T-PRO-X64ARM)+LCP-ANDROID-SERVER using TASKER <--> LCP-Server(Ubuntu-Server X64 VM)
-// READ MD for Prompts
-// 'Back Calls' or 'InstaIntents' will use MAKE API to save power. (Mb ZAPPIER)
-// Get limit loops around 60 mins.
+// Demo: Exchange data to power Smart-Home and IOT, between Android (Taker) and Ubuntu Server (node.js:express) 
+// REST API JSON-data. GET and POST 'Routes'
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const fs = require('fs');
 const app = express();
 const port = 3030;
-const bodyParser = require('body-parser');
-const { default: test } = require('node:test');
-//const { type } = require('os');
-//const path = require('path');
 
-// Define valid API keys
-const validApiKeys = ['3488', '3434', '4242'];
+// Define valid API keys:
+const validApiKeys = ['4345', '7496', '3423'];
 
-// Middleware to check API key
+// Middleware to check API key:
 const checkApiKey = (req, res, next) => {
   const apiKey = req.headers['api-key'] || req.query['api-key'];
   if (!apiKey) {
@@ -31,16 +27,11 @@ app.use(checkApiKey);
 
 app.use(bodyParser.text());
 
-app.get('/gett', (req, res) => {
-const gett = fs.readFileSync('./stats.txt', {flag: 'r'});
+app.get('/get', (req, res) => {
+const get = fs.readFileSync('./stats.txt', {flag: 'r'});
 res.send(txtString);
-
-});;
+});; // Connect and request (Get) from Android to 192.xxx.xxx.xxx:3030  (ubuntu)
   
-
-
-
-
 app.use(bodyParser.json());
 
 app.post('/post', (req, res) => {
@@ -48,11 +39,11 @@ app.post('/post', (req, res) => {
   console.log(data); // Access the JSON data from the request body
   res.send('Data received successfully!');
   const jsonString = JSON.stringify(data);
-  fs.writeFileSync("C:/Users/L0RD3/Documents/vs-code-workspace/project-x-workspace/data/in-payload.json", jsonString);
-});
+  fs.writeFileSync("/in-payload.json", jsonString);
+}); // Android send data object to txt file.
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-//Data loops (JSON reserve) and FLOW CONTROL next. 
+// Smartphone successfully exchanged data with server (Local Net)
